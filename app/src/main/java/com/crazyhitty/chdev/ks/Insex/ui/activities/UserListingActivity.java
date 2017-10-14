6391cee24a7ad00d4c83934052263e0393881143
novowebapp <novowebapp@gmail.com>
2017-10-14 13:30:17 -0300

    Initial revision

---
+++ app/src/main/java/com/crazyhitty/chdev/ks/Insex/ui/activities/UserListingActivity.java
@@ -0,0 +1,176 @@
+package com.crazyhitty.chdev.ks.Insex.ui.activities;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.crazyhitty.chdev.ks.firebasechat.R;
+import com.crazyhitty.chdev.ks.Insex.core.logout.LogoutContract;
+import com.crazyhitty.chdev.ks.Insex.core.logout.LogoutPresenter;
+import com.crazyhitty.chdev.ks.Insex.ui.adapters.UserListingPagerAdapter;
+
+public class UserListingActivity extends AppCompatActivity implements LogoutContract.View, NavigationView.OnNavigationItemSelectedListener {
+    private Toolbar mToolbar;
+    private TabLayout mTabLayoutUserListing;
+    private ViewPager mViewPagerUserListing;
+
+    private LogoutPresenter mLogoutPresenter;
+
+    public static void startActivity(Context context) {
+        Intent intent = new Intent(context, UserListingActivity.class);
+        context.startActivity(intent);
+    }
+
+    public static void startActivity(Context context, int flags) {
+        Intent intent = new Intent(context, UserListingActivity.class);
+        intent.setFlags(flags);
+        context.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.drawer_layout);
+        bindViews();
+        init();
+
+
+        //criando menu navigation drawer
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    private void bindViews() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mTabLayoutUserListing = (TabLayout) findViewById(R.id.tab_layout_user_listing);
+        mViewPagerUserListing = (ViewPager) findViewById(R.id.view_pager_user_listing);
+
+
+
+    }
+
+    private void init() {
+        // set the toolbar
+        setSupportActionBar(mToolbar);
+
+        // set the view pager adapter
+        UserListingPagerAdapter userListingPagerAdapter = new UserListingPagerAdapter(getSupportFragmentManager());
+        mViewPagerUserListing.setAdapter(userListingPagerAdapter);
+
+        // attach tab layout with view pager
+        mTabLayoutUserListing.setupWithViewPager(mViewPagerUserListing);
+
+        mLogoutPresenter = new LogoutPresenter(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            logout();
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.logout)
+                .setMessage(R.string.are_you_sure)
+                .setPositiveButton(R.string.logout, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        mLogoutPresenter.logout();
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .show();
+    }
+
+    @Override
+    public void onLogoutSuccess(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        LoginActivity.startIntent(this,
+                Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+    }
+
+    @Override
+    public void onLogoutFailure(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
